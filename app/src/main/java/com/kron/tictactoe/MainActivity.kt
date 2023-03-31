cf2767b06223730507291cac6afab8a44d7894d8
kron <smoya557@gmail.com>
2023-03-30 22:21:26 -0300

    initial commit w/ main screen layout done

--- app/src/main/java/com/kron/tictactoe/MainActivity.kt
+++ app/src/main/java/com/kron/tictactoe/MainActivity.kt
@@ -2,10 +2,14 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kron.tictactoe.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
